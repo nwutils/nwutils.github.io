f57d2770e44ee3f71b639eab6ea3a99de1de151b
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2021-05-01 20:34:50 -0400

    redesign

---
+++ components/learn-card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="learn-card">
+    <div class="learn-card-image-container">
+      <a :href="project.url" target="_blank">
+        <img
+          v-if="project.image"
+          :alt="project.title + ' preview image'"
+          :src="'_imgs/' + project.image"
+          class="learn-card-image"
+        >
+        <svg-icon v-else :icon="project.icon"></svg-icon>
+      </a>
+    </div>
+    <div class="learn-card-details">
+      <div>
+        <a :href="project.url" target="_blank">
+          {{ project.title }}
+        </a>
+      </div>
+      <div><strong>{{ project.author }}</strong></div>
+      <div><em>{{ project.site }}</em></div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'learn-card',
+  components: {
+    'svg-icon': httpVueLoader('components/svg-icon.vue')
+  },
+  props: {
+    project: {
+      type: Object,
+      required: false,
+      default: undefined
+    }
+  }
+};
+</script>
